b0ea038b29f4eb432a429b27669157295757ed19
vanes430 <vanessimbolon2020@gmail.com>
2026-05-17 17:35:02 +0700

    chore: stop tracking build, src-patched, and .gradle directories

--- src-patched/main/java/net/advancedplugins/ae/impl/effects/effects/mechanics/triggers/internal/UserRepeaters.java
+++
@@ -1,8 +0,0 @@
-package net.advancedplugins.ae.impl.effects.effects.mechanics.triggers.internal;
-
-import net.advancedplugins.ae.impl.utils.FoliaScheduler;
-import org.bukkit.inventory.EquipmentSlot;
-
-class UserRepeaters {
-   public final HashMap<EquipmentSlot, List<FoliaScheduler.Task>> itemRunnables = new HashMap<>();
-}
